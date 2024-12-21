f27297b0772c8e899572a268641f510842f45dfd
matthieugouel <matthieu.gouel@protonmail.com>
2024-12-21 17:50:46 +0100

    add TXT paramter and improved json response

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use chrono::Local;
 use clap::Parser as CliParser;
 use clap_verbosity_flag::{InfoLevel, Verbosity};
 use env_logger::Builder;
-use serde::Deserialize;
+use log::{error, info};
+use serde::{Deserialize, Serialize};
 use std::io::Write;
 
 use crate::porkbun::Porkbun;
@@ -65,15 +66,24 @@ struct Params {
     subdomain: String,
     a: Option<String>,
     aaaa: Option<String>,
+    txt: Option<String>,
     clear: Option<bool>,
 }
 
 use axum::response::Response as AxumResponse;
 
-#[derive(serde::Serialize)]
+#[derive(Deserialize, Serialize)]
+struct RecordResponse {
+    r#type: String,
+    content: String,
+}
+
+#[derive(Deserialize, Serialize)]
 struct Response {
     message: String,
     domain: String,
+    clear: bool,
+    records: Vec<RecordResponse>,
 }
 
 impl IntoResponse for Response {
@@ -95,54 +105,122 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn response(status: StatusCode, message: &str, domain: &str) -> (StatusCode, Json<Response>) {
+fn response(
+    status: StatusCode,
+    message: &str,
+    domain: &str,
+    records: Vec<(String, String)>,
+    clear: bool,
+) -> (StatusCode, Json<Response>) {
+    let mut record_responses = vec![];
+    for (record_type, record_content) in records {
+        record_responses.push(RecordResponse {
+            r#type: record_type,
+            content: record_content,
+        });
+    }
+
     (
         status,
         Json(Response {
             message: String::from(message),
             domain: String::from(domain),
+            records: record_responses,
+            clear: clear,
         }),
     )
 }
 
-#[axum::debug_handler]
-async fn root(State(cli): State<CLI>, params: Query<Params>) -> impl IntoResponse {
-    if cli.token != params.token {
-        return response(StatusCode::UNAUTHORIZED, "Unauthorized: Invalid token", "");
+async fn handle_record(
+    porkbun: &Porkbun,
+    subdomain: String,
+    record_type: String,
+    content: String,
+    clear: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    porkbun
+        .delete_record(&subdomain, &record_type, &content)
+        .await?;
+
+    if clear {
+        return Ok(());
     }
 
-    let (record_type, ip) = match (params.a.as_deref(), params.aaaa.as_deref()) {
-        (Some(a), None) => ("A", a),
-        (None, Some(aaaa)) => ("AAAA", aaaa),
-        _ => {
-            return response(
-                StatusCode::BAD_REQUEST,
-                "Bad request: Either A or AAAA record must be provided",
-                "",
-            )
-        }
-    };
+    porkbun
+        .create_record(&subdomain, &record_type, &content)
+        .await?;
 
+    Ok(())
+}
+
+#[axum::debug_handler]
+async fn root(State(cli): State<CLI>, params: Query<Params>) -> impl IntoResponse {
+    let is_clear = params.clear.unwrap_or(false);
+    if cli.token != params.token {
+        return response(
+            StatusCode::UNAUTHORIZED,
+            "Unauthorized: Invalid token",
+            "",
+            vec![],
+            is_clear,
+        );
+    }
+
+    let porkbun = Porkbun::new(
+        cli.porkbun_api_key,
+        cli.porkbun_secret_key,
+        cli.domain.clone(),
+    );
     let mut subdomain = params.subdomain.clone();
     if !cli.subdomain.is_none() {
         subdomain = format!("{}.{}", params.subdomain, cli.subdomain.unwrap());
     }
     let domain = format!("{}.{}", subdomain, cli.domain);
 
-    let porkbun = Porkbun::new(cli.porkbun_api_key, cli.porkbun_secret_key, cli.domain);
-    porkbun
-        .delete_record(&subdomain, &record_type, &ip)
-        .await
-        .unwrap();
-
-    if params.clear.unwrap_or(false) {
-        return response(StatusCode::OK, "OK", &domain);
+    let mut records = vec![];
+    if let Some(content) = params.a.clone() {
+        records.push((String::from("A"), content));
+    }
+    if let Some(content) = params.aaaa.clone() {
+        records.push((String::from("AAAA"), content));
+    }
+    if let Some(content) = params.txt.clone() {
+        records.push((String::from("TXT"), content));
     }
 
-    porkbun
-        .create_record(&subdomain, &record_type, &ip)
+    for (record_type, content) in records.clone().into_iter() {
+        match handle_record(
+            &porkbun,
+            subdomain.clone(),
+            record_type.clone(),
+            content.clone(),
+            is_clear,
+        )
         .await
-        .unwrap();
+        {
+            Ok(_) => {
+                let mut action = "updated";
+                if is_clear {
+                    action = "deleted"
+                }
 
-    response(StatusCode::OK, "OK", &domain)
+                info!("Record {}: {} {} {}", action, record_type, domain, content);
+            }
+            Err(e) => {
+                error!(
+                    "Error handling record: {} {} {}: {}",
+                    record_type, domain, content, e
+                );
+                return response(
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Internal Server Error",
+                    &domain,
+                    records.clone(),
+                    is_clear,
+                );
+            }
+        }
+    }
+
+    response(StatusCode::OK, "OK", &domain, records.clone(), is_clear)
 }
